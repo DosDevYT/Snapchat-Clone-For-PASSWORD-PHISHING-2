e58c7701b1c62b6c23c06d0aedf7a608a0aa74ec
Petr <legitkek@gmail.com>
2018-10-20 15:06:33 -0700

    Initial commit

---
+++ js/login.js
@@ -0,0 +1,9 @@
+(function() {
+  var $loginbutt = $('.js-loginbutt');
+  var $forgotpass = $('.js-forgotpass');
+  var $insertpass = $('.js-insertpass');
+  var $insertuser = $('.js-insertuser');
+
+
+  $forgotpass
+})();
